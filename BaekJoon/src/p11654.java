6788ef4bace3924df1bd038b40aa54d18250504b
cutegyuseok <yigyuseok@gmail.com>
2022-12-03 22:01:12 +0000

    p11654 done

---
+++ BaekJoon/src/p11654.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class p11654 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int ch = in.next().charAt(0);
+        System.out.print(ch);
+    }
+}
