b62505db6740d8d07f490cb9071eca3240e034be
gyuseok <103543611+cutegyuseok@users.noreply.github.com>
2022-12-15 17:15:22 +0900

    [Bronze III] Title: ACM 호텔, Time: 288 ms, Memory: 19404 KB -BaekjoonHub

---
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for (int t=0;t<T;t++){
+            int H= sc.nextInt();
+            int W=sc.nextInt();
+            int N=sc.nextInt();
+            int w =0;
+            int h=0;
+            if(N%H!=0){
+                w=N/H+1;
+                h=N%H;
+            }else {
+                w=N/H;
+                h=H;
+            }
+            System.out.println(h+String.format("%02d",w));
+        }
+    }
+}
